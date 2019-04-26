1b4366542b4c227144dd5a4a87adde48f0fbb2ba
Taras kuk <kukulyak.taras@gmail.com>
2019-04-26 12:07:55 +0300

    Finish initializator, start authentification

---
+++ 911Phone/Controllers/User/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Phone.Data.DTOs.User;
+using Phone.Data.Entities.User;
+using System.Threading.Tasks;
+
+namespace Phone.Controllers.User
+{
+
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private UserManager<ApplicationUser> userManager;
+
+        public AuthController(UserManager<ApplicationUser> user)
+        {
+            userManager = user;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<IActionResult> LoginAsync([FromBody]AuthLoginDto dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            ApplicationUser user = await userManager.FindByEmailAsync(dto.Email);
+
+            if (await userManager.CheckPasswordAsync(user, dto.Password))
+            {
+                return Ok("Success");
+            }
+
+            return BadRequest("Error");
+        }
+
+    }
+}
---
+++ 911Phone/Data/DTOs/User/AuthLoginDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Phone.Data.DTOs.User
+{
+    public class AuthLoginDto
+    {
+
+        [EmailAddress]
+        public virtual string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        public virtual string Password { get; set; }
+
+    }
+}
