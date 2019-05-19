4d571d03261d0369a67701bbb3f524b0ae2bf9c3
Taras kuk <kukulyak.taras@gmail.com>
2019-05-19 14:51:54 +0300

    Add admin, seller and customer controller tests

---
+++ PhoneUnitTests/User/Controllers/SellerControllerTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Phone.Controllers.User;
+using Phone.Data.DTOs.User;
+using Phone.Services.User.Interfaces;
+using PhoneUnitTests.User.Helpers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PhoneUnitTests.User.Controllers
+{
+    public class SellerControllerTest
+    {
+        private readonly Mock<ISellerService> mockSellerService;
+        private readonly Mock<UserViewDto> mockUserViewDto;
+        private readonly UserTestHelper userHelper;
+
+        public SellerControllerTest()
+        {
+            mockSellerService = new Mock<ISellerService>();
+            mockUserViewDto = new Mock<UserViewDto>();
+            userHelper = new UserTestHelper();
+        }
+
+        /// <summary>
+        /// Test for checking return list users with role - admin
+        /// <summary>
+        [Fact]
+        public async Task GetListAdminsTestAsync()
+        {
+            // Arrange
+            mockSellerService.Setup(service => service.ListSellersAsync()).ReturnsAsync(await userHelper.GetUsers());
+            SellerController controller = new SellerController(mockSellerService.Object);
+
+            // Act
+            var result = await controller.ListSellers();
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var model = Assert.IsType<List<UserViewDto>>(okResult.Value);
+
+            // Assert
+            Assert.Equal((await userHelper.GetUsers()).FirstOrDefault(f => f.Email == userHelper.EmailUser).Email, model.FirstOrDefault(f => f.Email == userHelper.EmailUser).Email);
+        }
+    }
+}
---
+++ PhoneUnitTests/User/Helpers/UserTestHelper.cs
@@ -0,0 +1,25 @@
+﻿using Phone.Data.Entities.User;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PhoneUnitTests.User.Helpers
+{
+    class UserTestHelper
+    {
+        public readonly string EmailUser = "user1@mail.ua";
+
+        public async Task<IList<ApplicationUser>> GetUsers()
+        {
+            return await Task.Run(() =>
+                new List<ApplicationUser>
+                {
+                    new ApplicationUser { Id="131d1b9d-6e7f-449a-9f7e-2b9769fd5001", Email="user1@mail.ua"},
+                    new ApplicationUser { Id="131d1b9d-6e7f-449a-9f7e-2b9769fd5002", Email="user2@mail.ua"},
+                    new ApplicationUser { Id="131d1b9d-6e7f-449a-9f7e-2b9769fd5003", Email="user3@mail.ua"},
+                    new ApplicationUser { Id="131d1b9d-6e7f-449a-9f7e-2b9769fd5004", Email="user4@mail.ua"}
+                }
+            );
+        }
+
+    }
+}
