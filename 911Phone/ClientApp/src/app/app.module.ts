78b71488271daae571849f07c82eb6d840450fca
Taras kuk <kukulyak.taras@gmail.com>
2019-06-28 12:32:56 +0300

    Finish Api and minimal settings ui

--- 911Phone/ClientApp/src/app/app.module.ts
+++ 911Phone/ClientApp/src/app/app.module.ts
@@ -1,25 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './site/login/login.component';
+import { ErrorComponent } from './site/error/error.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    LoginComponent,
+    ErrorComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    BrowserModule,
     HttpClientModule,
-    FormsModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' }
-    ])
+    RouterModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ PhoneUnitTests/Shop/Helpers/StorageTestHelper.cs
@@ -0,0 +1,18 @@
+﻿using Phone.Data.DTOs.Shop;
+using System.Threading.Tasks;
+
+namespace PhoneUnitTests.Shop.Helpers
+{
+    class StorageTestHelper
+    {
+        /// <summary>
+        /// Return list face data storage in like AddStorageDto 
+        /// <summary>
+        public static async Task<AddStorageDto> GetStorageCreateNormal()
+        {
+            return await Task.Run(() =>
+                new AddStorageDto { Count = 2, ShopId = 1, ProductId = 1 }
+            );
+        }
+    }
+}
